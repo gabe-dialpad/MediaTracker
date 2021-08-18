502333e0ebf7a8e49925de3e7372a63cbd0e7fd5
Gabriel Rosa <gabe.rosa@dialpad.com>
2021-08-18 18:00:02 -0300

    Add project initial files

---
+++ MediaTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaTracker
+//
+//  Created by Gabriel Rosa on 18/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
